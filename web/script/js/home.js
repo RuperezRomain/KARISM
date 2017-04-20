85ea6a77c7da4f09cb6c324540899f8f076c5b00
vien34 <vien34@vien34-Vostro-3559>
2017-04-20 14:27:19 +0200

    Revret Bug accueil.html.twig

--- web/script/js/home.js
+++ web/script/js/home.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     /*****header*****/
-
-    $(window).on('resize', function () {
-    });
+    $(window).on('resize', function () {});
 //    $(".sizeScreen").css("height", window.innerHeight - 100 + "px");
 
     $(document).ready(function () {
@@ -10,57 +8,58 @@ $(document).ready(function () {
     });
 
 //    Init SlickCarousel
+$(".titelHeader").css({
+    
+}); 
 
     $('.your-class').slick({
-     
 
- 
-  infinite: true,
-  speed: 300,
-  slidesToShow: 7,
-  slidesToScroll: 7,
-  arrows : false,
-  
-  responsive: [
-    {
-      breakpoint: 1000,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true
-        
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+        infinite: true,
+        speed: 300,
+        slidesToShow: 7,
+        slidesToScroll: 7,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true
+
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
 
     });
-    
+
     /*******Video****/
-     $(".vid").css({
+    $(".vid").css({
       position:'relative',
       height : window.innerHeight,
       paddingTop :100,
       paddingBottom :100
-      
+
     });
-   
+
 
 
 
