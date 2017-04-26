f13a903795564204d39015b287e48c814b3d5b04
vien34 <vien34@vien34-Vostro-3559>
2017-04-26 12:05:16 +0200

    Methode cheeck SerieDefault ok

--- src/AppBundle/Controller/ArtisteController.php
+++ src/AppBundle/Controller/ArtisteController.php
@@ -18,44 +18,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  *
  * @author vien34
  */
-class ArtisteController extends Controller{
-    
-    
-     /**
+class ArtisteController extends Controller {
+
+    /**
      * @Route("user/edit/role/role_artiste")
      */
     public function userAddrole_artiste() {
-       
-            // Recup id user courante
-            $em = $this->getDoctrine()->getManager();
-            $userId = $this->getUser()->getId();
-            $user = $this->getDoctrine()->getRepository(User::class)->find($userId);
- 
-      
+
+        // Recup id user courante
+        $em = $this->getDoctrine()->getManager();
+        $userId = $this->getUser()->getId();
+        $user = $this->getDoctrine()->getRepository(User::class)->find($userId);
+
+        // Check si la table Default pour cette user n'existe pas
+        $serieDefault = $em->getRepository(Serie::class)->findBy(array('name' => 'Default', 'userid' => $userId));
+        if ($serieDefault == null) {
+
             // Initalisation boolean ArtistValidate
             $user->setArtistValidate(0);
             $em->merge($user);
             $em->flush($user);
 
-           
-            // Creation serie par default
+            //Creation serie par default
             $serieDefault = new Serie();
             $serieDefault->setName("Default");
             $serieDefault->setUserid($user);
             $em->persist($serieDefault);
             $em->flush();
             
-            
+        } 
+
+
+//           
 //////////////////test Vue///////////////////
-            return $this->render('default/Perso/formCreateSerie.html.twig');
-        
+        return $this->render('default/Perso/formCreateSerie.html.twig');
     }
-    
+
     /**
      * @Route("user/edit/role/role_artiste")
      */
-    public function createSeriePicturs(){
+    public function createSeriePicturs() {
+        
         
         
     }
+
 }
