c6759109fa9dff571a05c120b11a8453ddd2eaf9
vien34 <vien34@vien34-Vostro-3559>
2017-04-27 22:33:34 +0200

    Suppression en sortie de la variable de session Suppression champ formulaire en fin de validation

--- web/script/js/artiste.js
+++ web/script/js/artiste.js
@@ -1,64 +1,75 @@
+//       Recuperation input formulaire
+var nomInput = $("input[name='appbundle_picture[nom]']");
+var styleInput = $("input[name$='appbundle_picture[style]']");
+var techInput = $("input[name$='appbundle_picture[techniques]']");
+var genreInput = $("input[name$='appbundle_picture[genres]']");
+var sizeInput = $("input[name$='appbundle_picture[size]']");
+var prixInput = $("input[name$='appbundle_picture[prix]']");
+var expoInput = $("input[name$='appbundle_picture[expos]']");
+var commInput = $("input[name$='appbundle_picture[commentaire]']");
+var imgInput = $("input[name$='appbundle_picture[img]']");
 
 $(document).ready(function () {
+
     getPicture();
+
     $(".validPicture").click(function () {
-
-
-        // init input Picture
-        $nomInput = $("input[name='nom']");
-        $styleInput = $("input[name$='style']");
-        $techInput = $("input[name$='tech']");
-        $genreInput = $("input[name$='genre']");
-        $sizeInput = $("input[name$='size']");
-        $prixInput = $("input[name$='prix']");
-        $expoInput = $("input[name$='expo']");
-        $commInput = $("input[name$='comm']");
-        $imgInput = $("input[name$='img']");
-
         getPicture();
-
-
         $("#contentPicture").empty();
-
     });
 
+    $("#exitSerie").click(function () {
 
+        $.ajax({
+            url: '/web/delete/cache/pictures',
+            type: 'Get',
+            dataType: 'json',
+            async: true,
+            success: function () {}
+
+        });
+
+    });
 });
-
 function getPicture() {
 /// Recuperation entity picture
-$.ajax({
-url: '/web/get/user/serie/pictures',
+    $.ajax({
+        url: '/web/get/user/serie/pictures',
         type: 'Get',
         dataType: 'json',
         async: true,
         success: function ($listePic) {
-
-
-
-        $($listePic).each(function () {
-
-
-
-
-$("#contentPicture").append("\
-<div class='col-sm-6 col-md-4'>\n\
-<div class='thumbnail'>\n\
-    <a href=''>\n\
-        <img src='/web/images/oeuvrePictures/"+this.img+"' alt=''>\n\
-    </a>\n\
-    <div class='caption'>\n\
-        <h3>"+this.nom+"</h3>\n\
-        <h4>"+this.commentaire+"</h>\n\
-        <div>\n\
-            <p>"+this.style+"</p>\n\
-            <p>"+this.cgenresommentaire+"</p>\n\
-        </div>\n\
-    </div>\n\
-    </div>\n\
-</div>");
+            //Supretion champ formulaire
+            nomInput.val('');
+            styleInput.val('');
+            techInput.val('');
+            techInput.val('');
+            sizeInput.val('');
+            prixInput.val('');
+            expoInput.val('');
+            commInput.val('');
+            imgInput.val('');
+            $($listePic).each(function () {
+                //Parce mes object picture
+                $("#contentPicture").append("\
+                <div class='col-sm-6 col-md-4'>\n\
+                    <div class='thumbnail'>\n\
+                        <a href=''>\n\
+                            <img src='/web/images/oeuvrePictures/" + this.img + "' alt=''>\n\
+                        </a>\n\
+                        <div class='caption'>\n\
+                            <h3>" + this.nom + "</h3>\n\
+                            <h4>" + this.commentaire + "</h>\n\
+                            <div>\n\
+                                <p>" + this.style + "</p>\n\
+                                <p>" + this.genres + "</p>\n\
+                            </div>\n\
+                        </div>\n\
+                    </div>\n\
+                </div>");
 //
-});
-}
-});
+            });
+        }
+    });
+
 }
