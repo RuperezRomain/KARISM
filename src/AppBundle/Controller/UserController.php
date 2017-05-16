c2fd5a44b4f062f4c74db58300815ed40bb6b605
RuperezRomain <ruperez.dev@gmail.com>
2017-05-16 16:37:27 +0200

    method for wishlist view for wishlist

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Picture;
 use AppBundle\Entity\Place;
 use AppBundle\Entity\Serie;
 use AppBundle\Entity\User;
+use AppBundle\Entity\Wishlist;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -65,8 +66,32 @@ class UserController extends Controller {
         $user = $this->getDoctrine()->getRepository(User::class)->find($id);
         $series = $this->getDoctrine()->getRepository(Serie::class)->findByUserid($id);
         $places = $this->getDoctrine()->getRepository(Place::class)->findBy(array('fk_user' => $id));
-
-        return $this->render('default/profil.html.twig', array("user" => $user, "places" => $places, "series" => $series));
+        
+        $em = $this->getDoctrine()->getManager();
+       $userId = $user->getId();
+        $wishlist = $em->getRepository(Wishlist::class)->findByUsermain(array("usermain" => $userId));
+        $wishlisted = $em->getRepository(Wishlist::class)->findByArtiste(array("artiste" => $userId));
+        
+                $wishlistArtiste = array();
+        for($i=0; $i< count($wishlist); $i++){
+            array_push($wishlistArtiste, $wishlist[$i]);
+        }        
+                $wislistedArtiste = array();
+        for($i=0; $i< count($wishlisted); $i++){
+            array_push($wislistedArtiste, $wishlisted[$i]);
+//        echo($wislistedArtiste[$i]);
+        }        
+        
+        $userArtiste = $user->getRoles();
+        $this->get('session')->remove("userProfil");
+        for ($i=0; $i< count($userArtiste); $i++){
+            if ($userArtiste[$i]->getRole() == "ROLE_ARTISTE"){
+                $ok = "ok";
+                $this->get('session')->set("userProfil", $ok);
+            }
+        }
+        
+        return $this->render('default/profil.html.twig', array("user" => $user, "places" => $places, "series" => $series, "wishlistArtiste" => $wishlistArtiste, "wislistedArtiste" => $wislistedArtiste));
     }
 
 }
